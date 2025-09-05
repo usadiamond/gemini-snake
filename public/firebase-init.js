e54efb08559f0dba4f131d8d1d156ef55f3b058c
usadiamond <128734751+usadiamond@users.noreply.github.com>
2025-09-04 23:11:07 -0700

    Create firebase-init.js

---
+++ public/firebase-init.js
@@ -0,0 +1,10 @@
+// IMPORTANT: replace the <...> placeholders with the values you get from
+// Firebase console -> Project settings -> Your web app config.
+window.firebaseConfig = {
+  apiKey: "<YOUR_FIREBASE_API_KEY>",
+  authDomain: "<YOUR_PROJECT>.firebaseapp.com",
+  projectId: "<YOUR_PROJECT>",
+  storageBucket: "<YOUR_PROJECT>.appspot.com",
+  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>",
+  appId: "<YOUR_APP_ID>"
+};
