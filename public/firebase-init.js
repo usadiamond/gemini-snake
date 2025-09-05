a9a9df1308183dfd932b31dcd5615ad0ae4bc942
usadiamond <128734751+usadiamond@users.noreply.github.com>
2025-09-04 23:12:44 -0700

    Update firebase-init.js

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -1,10 +1,12 @@
 // IMPORTANT: replace the <...> placeholders with the values you get from
 // Firebase console -> Project settings -> Your web app config.
 window.firebaseConfig = {
-  apiKey: "<YOUR_FIREBASE_API_KEY>",
-  authDomain: "<YOUR_PROJECT>.firebaseapp.com",
-  projectId: "<YOUR_PROJECT>",
-  storageBucket: "<YOUR_PROJECT>.appspot.com",
-  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>",
-  appId: "<YOUR_APP_ID>"
+  apiKey: "AIzaSyApY5vIrS-by7VTZnma7TWdnyoUOohr08g",
+  authDomain: "snake-447a3.firebaseapp.com",
+  databaseURL: "https://snake-447a3-default-rtdb.firebaseio.com",
+  projectId: "snake-447a3",
+  storageBucket: "snake-447a3.firebasestorage.app",
+  messagingSenderId: "962445311597",
+  appId: "1:962445311597:web:aa6676df486f7bd77a4964",
+  measurementId: "G-XWT29YE235"
 };
